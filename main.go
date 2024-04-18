acf98132ecf91fe5bf45ec792a16536146e93617
static-fuji <jyun1228haru@icloud.com>
2024-04-18 19:27:47 +0900

    [update]パスの判別機能を実装

--- main.go
+++ main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	key := r.URL.Path[len("/objects/"):]
+
+	validKey := regexp.MustCompile(`^[a-zA-Z0-9]{1,10}$`)
+	if !validKey.MatchString(key) {
+		http.Error(w, "404", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "アクセスキー:%s", key)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/objects/", handler)
 	http.ListenAndServe(":8000", nil)
+
+	fmt.Println("サーバーを起動します... ポート 8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		fmt.Println("サーバーの起動に失敗しました:", err)
+	}
 }
