b9b2c82b0f3e92b5bd08c98f3ab27d787dd9d8d9
static-fuji <jyun1228haru@icloud.com>
2024-04-19 12:21:12 +0900

    [update] リクエストヘッダがGETのときの処理を追加

--- main.go
+++ main.go
@@ -4,19 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"regexp"
+	"sync"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	key := r.URL.Path[len("/objects/"):]
-
-	validKey := regexp.MustCompile(`^[a-zA-Z0-9]{1,10}$`)
-	if !validKey.MatchString(key) {
-		http.Error(w, "404", http.StatusNotFound)
-		return
-	}
-
-	fmt.Fprintf(w, "アクセスキー:%s", key)
-}
+var (
+	data  map[string][]byte
+	mutex sync.RWMutex
+)
 
 func main() {
 	http.HandleFunc("/objects/", handler)
@@ -27,4 +21,42 @@ func main() {
 	if err != nil {
 		fmt.Println("サーバーの起動に失敗しました:", err)
 	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Path[len("/objects/"):]
+
+	validKey := regexp.MustCompile(`^[a-zA-Z0-9]{1,10}$`)
+	if !validKey.MatchString(key) {
+		http.Error(w, "404, Not Found", http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodPut:
+		putData(w, r, key)
+	case http.MethodGet:
+		getData(w, r, key)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
+	fmt.Fprintf(w, "アクセスキー:%s", key)
+}
+
+func putData(w http.ResponseWriter, r *http.Request, key string) {
+	body := make([]byte, r.ContentLength)
+	_, err := r.Body.Read(body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+
+	mutex.Lock()
+	defer mutex.Unlock()
+	data[key] = body
+}
+
+func getData(w http.ResponseWriter, r *http.Request, key string) {
+
 }
