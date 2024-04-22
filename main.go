97ed9c41a1ce6bce43cefae5161ffc881d1fcc75
static-fuji <jyun1228haru@icloud.com>
2024-04-22 13:46:54 +0900

    [fix] コメントの修正

--- main.go
+++ main.go
@@ -24,7 +24,6 @@ func main() {
 	http.HandleFunc("/objects/", m.handler)
 	http.ListenAndServe(":8000", nil)
 
-	fmt.Println("サーバーを起動します... ポート 8000")
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		fmt.Println("サーバーの起動に失敗しました:", err)
@@ -51,39 +50,32 @@ func (m *dataBody) handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *dataBody) putData(w http.ResponseWriter, r *http.Request, key string) {
-	//body := make([]byte, r.ContentLength)
+
 	fmt.Println(m.data[key])
 	body, err := io.ReadAll(r.Body)
-
-	//mapの初期化
 
 	if err != nil {
 		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
 		return
 	}
 
-	//data := make(map[string][]byte)
-
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 	m.data[key] = body
-	fmt.Println(m.data[key])
 }
 
 func (m *dataBody) getData(w http.ResponseWriter, r *http.Request, key string) {
-	fmt.Println(m.data[key])
-
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 
-	// データを取得
+	//レスポンスボディの作成
 	data, ok := m.data[key]
 	if !ok {
 		http.NotFound(w, r)
 		return
 	}
 
-	// レスポンスにデータを書き込み
+	//レスポンスの書き込み
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
 }
